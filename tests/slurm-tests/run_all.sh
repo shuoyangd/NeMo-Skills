e7b8bf3371f0d09ff65b2fd30c3193cb9e092c37
Igor Gitman <igitman@nvidia.com>
2025-08-30 09:45:58 -0700

    Remove wandb_project
    Signed-off-by: Igor Gitman <igitman@nvidia.com>

--- tests/slurm-tests/run_all.sh
+++ tests/slurm-tests/run_all.sh
@@ -5,6 +5,6 @@ CLUSTER=$1
 CURRENT_DATE=$(date +%Y-%m-%d)
 
 python tests/slurm-tests/super_49b_evals/run_test.py --cluster $CLUSTER --workspace /workspace/nemo-skills-slurm-ci/$CURRENT_DATE/super_49b_evals --expname_prefix $CURRENT_DATE &
-python tests/slurm-tests/omr_simple_recipe/run_test.py --cluster $CLUSTER --backend nemo-aligner --workspace /workspace/nemo-skills-slurm-ci/$CURRENT_DATE/omr_simple_recipe/nemo-aligner --wandb_project nemo-skills-slurm-ci --expname_prefix $CURRENT_DATE-omr-simple-recipe-nemo-aligner &
-python tests/slurm-tests/omr_simple_recipe/run_test.py --cluster $CLUSTER --backend nemo-rl --workspace /workspace/nemo-skills-slurm-ci/$CURRENT_DATE/omr_simple_recipe/nemo-rl --wandb_project nemo-skills-slurm-ci --expname_prefix $CURRENT_DATE-omr-simple-recipe-nemo-rl &
+python tests/slurm-tests/omr_simple_recipe/run_test.py --cluster $CLUSTER --backend nemo-aligner --workspace /workspace/nemo-skills-slurm-ci/$CURRENT_DATE/omr_simple_recipe/nemo-aligner --expname_prefix $CURRENT_DATE-omr-simple-recipe-nemo-aligner &
+python tests/slurm-tests/omr_simple_recipe/run_test.py --cluster $CLUSTER --backend nemo-rl --workspace /workspace/nemo-skills-slurm-ci/$CURRENT_DATE/omr_simple_recipe/nemo-rl --expname_prefix $CURRENT_DATE-omr-simple-recipe-nemo-rl &
 wait
